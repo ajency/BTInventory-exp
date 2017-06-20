9f2b6b99d24c30555a4bb1326e12a0b3227b53f8
Antonio Furtado <antonio@ajency.in>
2017-06-20 15:41:42 +0530

    Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { InventoryApp } from './app.component';
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    InventoryApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(InventoryApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    InventoryApp
   ],
   providers: [
     StatusBar,

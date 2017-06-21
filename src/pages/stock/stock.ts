d36f307ed96505e4e91bdecf2f25a495cb8c296f
jeromie <jeromie@ajency.in>
2017-06-21 18:30:30 +0530

    ui fixes

--- src/pages/stock/stock.ts
+++ src/pages/stock/stock.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Location } from '@angular/common';
 
 /**
  * Generated class for the StockPage page.
@@ -14,11 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StockPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private location: Location) {
   }
+
+  private productList: any = [];
+
+  private dummyProducts = [];
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad StockPage');
+  }
+
+  private itemtMouseOver(item: any): void{
+    item.open = true;
+    // item.selectedDetail = item.selectedDetail ? item.selectedDetail : 'seller';
+  }
+
+  private itemMouseOut(item: any): void{
+    if(!item.selectedDetail){
+          item.open = false
+    }
+    // item.selectedDetail = null;
+  }
+
+  private goBack(): void{
+    this.location.back();
   }
 
   private paginationConfig: any = {
